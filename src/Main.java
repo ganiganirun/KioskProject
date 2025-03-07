a68d272e0c9c33fa8b0202f62e171a0543d5ae3e
songgaeun <rkdmsdmqek1128@gmail.com>
2025-03-07 16:50:24 +0900

    style: 메뉴판 출력 로직 변경
    단순히 print문으로 찍어주었던 메뉴판을 에러이리스트에서 값을 가져와 출력해주는 로직으로 변경

--- src/Main.java
+++ src/Main.java
@@ -15,16 +15,20 @@ public class Main {
 
         List<MenuItem> menuItemList = new ArrayList<>();
 
-        menuItemList.add(new MenuItem("Big Mac® Meal",6300,"참깨빵위에 순쇠고기 패티 두장 특별한 소스 양상추 치즈 피클 양파까지~"));
-        menuItemList.add(new MenuItem("McSpicy® Shanghai Burger Meal",6300,"쌀가루가 더해져 더 바삭해진 100% 닭가슴살 패티가 들어간 버거"));
-        menuItemList.add(new MenuItem("McCrispy™ Deluxe Burger Meal",7500,"100% 통닭다리살 겉바속촉 케이준 치킨 패티에 브리오쉬 번을 사용한 버거"));
-        menuItemList.add(new MenuItem("1955® Burger Meal",7000,"10 : 1 패티와 그릴드 어니언까지 맥도날드가 처음 생긴 1955년의 맛을 담은 버거"));
+        menuItemList.add(new MenuItem("Big Mac® Meal                            ",6300,"참깨빵위에 순쇠고기 패티 두장 특별한 소스 양상추 치즈 피클 양파까지~"));
+        menuItemList.add(new MenuItem("McSpicy® Shanghai Burger Meal            ",6300,"쌀가루가 더해져 더 바삭해진 100% 닭가슴살 패티가 들어간 버거"));
+        menuItemList.add(new MenuItem("McCrispy™ Deluxe Burger Meal             ",7500,"100% 통닭다리살 겉바속촉 케이준 치킨 패티에 브리오쉬 번을 사용한 버거"));
+        menuItemList.add(new MenuItem("1955® Burger Meal                        ",7000,"10 : 1 패티와 그릴드 어니언까지 맥도날드가 처음 생긴 1955년의 맛을 담은 버거"));
         menuItemList.add(new MenuItem("McSpicy® Shanghai Spicy Creamy Sauce Meal",8200,"쌀가루가 더해져 더 바삭해진 100% 닭가슴살 패티에 부드러운 크림 베이스에 매콤함이 가득!"));
-        menuItemList.add(new MenuItem("Shrimp Beef Spicy Creamy Sauce Meal",8600,"탱글한 통 새우살에 비프 패티에 부드러운 크림 베이스에 깊은 매콤함이 가득!"));
-
+        menuItemList.add(new MenuItem("Shrimp Beef Spicy Creamy Sauce Meal      ",8600,"탱글한 통 새우살에 비프 패티에 부드러운 크림 베이스에 깊은 매콤함이 가득!"));
 
         while (flag){
             System.out.println("[ McDonald Lunch Menu]");
+            int i = 1;
+            for(MenuItem menuItem : menuItemList){
+                System.out.println(i + ". " + menuItem.getBuger() + " | " + menuItem.getPrice() + "원 | " + menuItem.getExplan());
+                i += 1;
+            }
 
             System.out.println("0. 키오스크 종료\n");
 
