d70f5521c40dac9feddcd76446c3c4b96ae3b6d7
songgaeun <rkdmsdmqek1128@gmail.com>
2025-03-07 16:22:55 +0900

    feat: MenuItem 생성 및 구현
    MenuItem 클래스 생성 및 속성, 생성자, 기능을 정의해줌

---
+++ src/MenuItem.java
@@ -0,0 +1,28 @@
+public class MenuItem {
+    // 속성
+    String buger;
+    long price;
+    String explan;
+
+    // 생성자
+    public MenuItem(String buger, long price, String explan){
+        this.buger = buger;
+        this.price = price;
+        this.explan = explan;
+    }
+
+    // 기능
+    public String getBuger(){
+        return buger;
+    }
+
+    public long getPrice(){
+        return price;
+    }
+
+    public String getExplan(){
+        return explan;
+    }
+
+
+}
